d061b87168d735be48d40ee080f9d7474685a210
jordanonuma <jordanonuma@outlook.com>
2019-11-01 06:11:27 -1000

    Added struct Zombie[] that will contain zombie's name and DNA.

--- ZombieFactory.sol
+++ ZombieFactory.sol
@@ -3,4 +3,9 @@ pragma solidity ^0.4.25;
 contract ZombieFactory {
     uint dnaDigits = 16;
     uint dnaModulus = 10 ** dnaDigits;
-} //end Contract ZombieFactory {}
+
+    struct Zombie {
+        string name;
+        uint dna;
+    } // end struct Zombie {}
+} //end contract ZombieFactory {}
