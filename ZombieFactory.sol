7bacf116ca7329e4a5115940c802e3763c2ce8d2
jordanonuma <jordanonuma@outlook.com>
2019-11-11 11:47:34 -1000

    Incremented the zombie count given the user.

--- ZombieFactory.sol
+++ ZombieFactory.sol
@@ -18,6 +18,7 @@ contract ZombieFactory {
     function _createZombie(string _name, uint _dna) private {
         uint id = zombies.push(Zombie(_name, _dna)) - 1;
         zombieToOwner[id] = msg.sender;
+        ownerZombieCount[msg.sender]++;
         emit NewZombie(id, _name, _dna);
     } //end function createZombie()
 
