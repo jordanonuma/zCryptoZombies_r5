f898f5702f17d28341881e26a155d598595527b5
jordanonuma <jordanonuma@outlook.com>
2019-11-12 05:30:53 -1000

    Added requirement that user can only call the function creating a zombie once.

--- ZombieFactory.sol
+++ ZombieFactory.sol
@@ -28,6 +28,7 @@ contract ZombieFactory {
     } //end function _generateRandomDna()
 
     function createRandomZombie(string _name) public {
+        require(ownerZombieCount[msg.sender] == 0);
         uint randDna = _generateRandomDna(_name);
         _createZombie(_name, randDna);
     } //end function createRandomZombie()
