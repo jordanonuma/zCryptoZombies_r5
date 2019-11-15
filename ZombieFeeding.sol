ecfedc8dd2834c7c99577cbc7d087f62d407a13f
jordanonuma <jordanonuma@outlook.com>
2019-11-15 06:17:02 -1000

    Added a local instance of the Zombie[] struct called 'myZombie'. This was taken from the 'zombies' array and is stored on chain.

--- ZombieFeeding.sol
+++ ZombieFeeding.sol
@@ -4,5 +4,6 @@ import "./ZombieFactory.sol";
 contract ZombieFeeding is ZombieFactory {
     function feedAndMultiply(uint _zombieId, uint _targetDna) public {
       require(msg.sender == zombieToOwner[_zombieId]);
+      Zombie storage myZombie = zombies[_zombieId];
     } //end function feedAndMultiply()
 } //end contract ZombieFeeding {}
