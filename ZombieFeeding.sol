7387f7f7938a46a5f38d7a162572bdec6cc3fdd0
jordanonuma <jordanonuma@outlook.com>
2019-11-15 06:13:49 -1000

    Added frame for function that will allow user's zombie to attack other zombies.

--- ZombieFeeding.sol
+++ ZombieFeeding.sol
@@ -2,5 +2,7 @@ pragma solidity ^0.4.25;
 import "./ZombieFactory.sol";
 
 contract ZombieFeeding is ZombieFactory {
-    
+    function feedAndMultiply(uint _zombieId, uint _targetDna) public {
+      
+    } //end function feedAndMultiply()
 } //end contract ZombieFeeding {}
