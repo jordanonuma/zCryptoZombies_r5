2d4c8790887ffc2383787ccb2a009879edee18a2
jordanonuma <jordanonuma@outlook.com>
2019-11-18 06:23:55 -1000

    Added frame of KittyInterface. This allows to call the function returning the kitty's genes.

--- ZombieFeeding.sol
+++ ZombieFeeding.sol
@@ -1,6 +1,10 @@
 pragma solidity ^0.4.25;
 import "./ZombieFactory.sol";
 
+contract KittyInterface {
+  
+} //end contract KittyInterface {}
+
 contract ZombieFeeding is ZombieFactory {
     function feedAndMultiply(uint _zombieId, uint _targetDna) public {
       require(msg.sender == zombieToOwner[_zombieId]);
