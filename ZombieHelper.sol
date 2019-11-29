12a64f4d7d7785f5695981bcaafce98f510dbc99
jordanonuma <jordanonuma@outlook.com>
2019-11-29 11:11:10 -1000

    Added frame for modifier that checks zombie's level meets the minimum.

--- ZombieHelper.sol
+++ ZombieHelper.sol
@@ -2,5 +2,7 @@ pragma solidity ^0.4.25;
 import "./ZombieFeeding.sol"
 
 contract ZombieHelper is ZombieFeeding {
-
+  modifier aboveLevel(uint _level, uint _zombieId) {
+      
+  } //end modifier aboveLevel()
 } //end contract ZombieHelper{}
