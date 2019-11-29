e2fe12f16d29084a83213df33f0e5c0622533486
jordanonuma <jordanonuma@outlook.com>
2019-11-29 11:12:07 -1000

    Added boolean requirement.

--- ZombieHelper.sol
+++ ZombieHelper.sol
@@ -3,6 +3,7 @@ import "./ZombieFeeding.sol"
 
 contract ZombieHelper is ZombieFeeding {
   modifier aboveLevel(uint _level, uint _zombieId) {
-      
+    require(zombies[_zombieId].level >= _level);
+    _;
   } //end modifier aboveLevel()
 } //end contract ZombieHelper{}
