be62bde6c93e28e0937af1ead53751ec59cce4ee
jordanonuma <jordanonuma@outlook.com>
2019-11-30 10:06:28 -1000

    Revised for syntax.

--- ZombieHelper.sol
+++ ZombieHelper.sol
@@ -8,12 +8,12 @@ contract ZombieHelper is ZombieFeeding {
   } //end modifier aboveLevel()
 
   function changeName(uint _zombieId, string _newName) external aboveLevel(2, _zombieId) {
-    require(msg.sender = zombieToOwner[_zombieId]); //how is different than using onlyOwner?
+    require(msg.sender == zombieToOwner[_zombieId]); //how is different than using onlyOwner?
     zombies[_zombieId].name = _newName;
   } //end function changeName()
 
   function changeDna(uint _zombieId, uint _newDna) external aboveLevel(20, _zombieId) {
-    require(msg.sender = zombieToOwner[_zombieId]);
+    require(msg.sender == zombieToOwner[_zombieId]);
     zombies[_zombieId].dna = _newDna;
   } //end function changeDna()
 } //end contract ZombieHelper{}
