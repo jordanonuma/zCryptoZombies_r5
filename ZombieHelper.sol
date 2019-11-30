0954256939c39bf7a45fa215675ea344e4c7ab95
jordanonuma <jordanonuma@outlook.com>
2019-11-30 10:01:59 -1000

    Set new name.

--- ZombieHelper.sol
+++ ZombieHelper.sol
@@ -8,6 +8,7 @@ contract ZombieHelper is ZombieFeeding {
   } //end modifier aboveLevel()
 
   function changeName(uint _zombieId, string _newName) external aboveLevel(2, _zombieId) {
-    require(msg.sender = zombieToOwner[_zombieId]);
+    require(msg.sender = zombieToOwner[_zombieId]); //how is different than using onlyOwner?
+    zombies[_zombieId].name = _newName;
   } //end function changeName()
 } //end contract ZombieHelper{}
