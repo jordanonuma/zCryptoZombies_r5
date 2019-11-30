5e1fc93168bc9c82a57bb2fc6402d7cf5d0bbee0
jordanonuma <jordanonuma@outlook.com>
2019-11-30 10:01:05 -1000

    Added requirement that user is owner's zombie.

--- ZombieHelper.sol
+++ ZombieHelper.sol
@@ -8,6 +8,6 @@ contract ZombieHelper is ZombieFeeding {
   } //end modifier aboveLevel()
 
   function changeName(uint _zombieId, string _newName) external aboveLevel(2, _zombieId) {
-      
+    require(msg.sender = zombieToOwner[_zombieId]);
   } //end function changeName()
 } //end contract ZombieHelper{}
