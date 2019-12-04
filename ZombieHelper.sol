36edd88ef0e83dd79cce58a66af91eb80f858d22
jordanonuma <jordanonuma@outlook.com>
2019-12-04 06:18:17 -1000

    Defined the feel for user to level up their zombie.

--- ZombieHelper.sol
+++ ZombieHelper.sol
@@ -2,6 +2,8 @@ pragma solidity ^0.4.25;
 import "./ZombieFeeding.sol"
 
 contract ZombieHelper is ZombieFeeding {
+  uint levelUpFee = 0.001 ether;
+
   modifier aboveLevel(uint _level, uint _zombieId) {
     require(zombies[_zombieId].level >= _level);
     _;
