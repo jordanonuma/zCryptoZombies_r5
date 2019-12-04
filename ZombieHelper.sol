119aa45a1f3db82d069c348e36dbe71e1e11309b
jordanonuma <jordanonuma@outlook.com>
2019-12-04 06:21:01 -1000

    Added increment.

--- ZombieHelper.sol
+++ ZombieHelper.sol
@@ -11,7 +11,7 @@ contract ZombieHelper is ZombieFeeding {
 
   function levelUp(uint _zombieId) external payable {
     require(msg.value == levelUpFee);
-    
+    zombies[_zombieId].level++;
   } //end function levelUp()
 
   function changeName(uint _zombieId, string _newName) external aboveLevel(2, _zombieId) {
