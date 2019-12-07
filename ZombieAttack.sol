af92865291a60e4fdf26ebf4be92f359749dd156
jordanonuma <jordanonuma@outlook.com>
2019-12-07 13:01:32 -1000

    Set the nonce and added frame for function that will return a pseudo-random number based on the keccak256 hash.

--- ZombieAttack.sol
+++ ZombieAttack.sol
@@ -2,5 +2,9 @@ pragma solidity >=0.5.0 <0.6.0; //not sure on the deviation from ^0.4.25
 import "./zombiehelper.sol";
 
 contract ZombieAttack is ZombieHelper {
+  uint randNonce = 0;
 
+  function randMod(uint _modulus) internal returns(uint) {
+    
+  } //end function randMod()
 } //end contract ZombieAttack{}
