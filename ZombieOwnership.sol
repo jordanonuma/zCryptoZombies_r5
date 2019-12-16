864c890e04a8cc55e8dd0c5bae2c153b2e93aab7
jordanonuma <jordanonuma@outlook.com>
2019-12-16 05:47:54 -1000

    Imported and added inherited ERC721.sol.

--- ZombieOwnership.sol
+++ ZombieOwnership.sol
@@ -1,6 +1,7 @@
 pragma solidity ^0.4.25;
 import "./zombieattack.sol";
+import "./ERC721.sol";
 
-contract ZombieOwnership is ZombieAttack {
+contract ZombieOwnership is ZombieAttack, ERC721 {
     
 } //end contract ZombieOwnership {}
