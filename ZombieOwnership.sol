61395ba9acfe992ac96f14a2401d513b0dd9c9a7
jordanonuma <jordanonuma@outlook.com>
2019-12-17 05:41:41 -1000

    Added frame of function that returns zombie's owner.

--- ZombieOwnership.sol
+++ ZombieOwnership.sol
@@ -6,4 +6,8 @@ contract ZombieOwnership is ZombieAttack, ERC721 {
   function balanceOf(address _owner) external view returns (uint256) {
     
   } //end function balanceOf()
+
+  function ownerOf(uint256 _tokenId) external view returns (address) {
+    
+  } //end function ownerOf()
 } //end contract ZombieOwnership {}
