2092f18a1ed49813d0befc86f420292b90f43c3a
jordanonuma <jordanonuma@outlook.com>
2019-12-17 05:41:00 -1000

    Added frame of function that returns how many zombies user has.

--- ZombieOwnership.sol
+++ ZombieOwnership.sol
@@ -3,5 +3,7 @@ import "./zombieattack.sol";
 import "./ERC721.sol";
 
 contract ZombieOwnership is ZombieAttack, ERC721 {
+  function balanceOf(address _owner) external view returns (uint256) {
     
+  } //end function balanceOf()
 } //end contract ZombieOwnership {}
