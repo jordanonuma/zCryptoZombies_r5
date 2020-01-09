79ed6635e2fbce28f85572768463e756c3d24734
jordanonuma <jordanonuma@outlook.com>
2020-01-09 05:49:28 -1000

    Set player2's defense level to 15.

--- zb_game_mode-master/contracts/Core/ZB/ExampleGame.sol
+++ zb_game_mode-master/contracts/Core/ZB/ExampleGame.sol
@@ -10,6 +10,9 @@ contract ExampleGame is ZBGameMode  {
 
         ZBSerializer.SerializedGameStateChanges memory changes;
         changes.init();
+
+        //Sets pre-match game states of players
+        changes.changePlayerDefense(Player.Player2, 15);
     } //end function beforeMatchStart()
 
 } //end contract ExampleGame {}
