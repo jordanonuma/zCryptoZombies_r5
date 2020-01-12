f7b803e631dbe5e53844e782befb74b696854881
jordanonuma <jordanonuma@outlook.com>
2020-01-12 11:57:06 -1000

    Copied card over to new array.

--- zb_game_mode-master/contracts/Core/ZB/ExampleGame.sol
+++ zb_game_mode-master/contracts/Core/ZB/ExampleGame.sol
@@ -57,7 +57,7 @@ contract ExampleGame is ZBGameMode  {
             
             for (uint j = 0; j < gameState.playerStates[i].cardsInDeck.length; j++) {
                 if isLegalCard(gameState.playerStates[i].cardsInDeck[j]) {
-                    
+                    newCards[cardCount] = gameState.playerStates[i].cardsInDeck[j];
                 } //end if()
             } //end for (uint j)
         } //end for (uint i)
