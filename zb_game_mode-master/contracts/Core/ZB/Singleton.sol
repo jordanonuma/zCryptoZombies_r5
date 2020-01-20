c20b2741dc22c740b4e65a8b48312aca97ce5506
jordanonuma <jordanonuma@outlook.com>
2020-01-20 06:17:35 -1000

    If card exists in player's deck the duplicate flag is set to true.

--- zb_game_mode-master/contracts/Core/ZB/Singleton.sol
+++ zb_game_mode-master/contracts/Core/ZB/Singleton.sol
@@ -49,7 +49,7 @@ contract Singleton is ZBGameMode  {
 
                 for (uint k = 0; k < cardCount; k++) {
                      if (keccak256(abi.encodePacked(newCards[k].mouldName)) == keccak256(abi.encodePacked(gameState.playerStates[i].cardsInDeck[j].mouldName))){
-                        
+                        cardAlreadyInDeck = true;
                     }
                 } //end for(uint k)
 
