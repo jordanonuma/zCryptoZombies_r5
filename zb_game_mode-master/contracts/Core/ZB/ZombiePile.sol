d45e7426d0b441bf84fcc614da912349b770ceba
jordanonuma <jordanonuma@outlook.com>
2020-01-21 06:21:14 -1000

    Declared cardInstance[] for each player.

--- zb_game_mode-master/contracts/Core/ZB/ZombiePile.sol
+++ zb_game_mode-master/contracts/Core/ZB/ZombiePile.sol
@@ -39,6 +39,9 @@ contract ZombiePile is ZBGameMode  {
 
         ZBSerializer.SerializedGameStateChanges memory changes;
         changes.init();
+    
+        CardInstance[] memory player1Cards = new CardInstance[](gameState.playerStates[0].cardsInDeck.length);
+        CardInstance[] memory player2Cards = new CardInstance[](gameState.playerStates[1].cardsInDeck.length);
 
         for (uint i=0; i < gameState.playerStates.length; i++) {
             for (uint j = 0; j < gameState.playerStates[i].cardsInDeck.length; j++) {
