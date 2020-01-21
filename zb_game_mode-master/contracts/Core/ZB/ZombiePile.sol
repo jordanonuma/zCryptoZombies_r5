e620e39a914b7310b148d949142565c584b187fb
jordanonuma <jordanonuma@outlook.com>
2020-01-21 06:22:59 -1000

    Revised spelling.

--- zb_game_mode-master/contracts/Core/ZB/ZombiePile.sol
+++ zb_game_mode-master/contracts/Core/ZB/ZombiePile.sol
@@ -42,8 +42,8 @@ contract ZombiePile is ZBGameMode  {
     
         CardInstance[] memory player1Cards = new CardInstance[](gameState.playerStates[0].cardsInDeck.length);
         CardInstance[] memory player2Cards = new CardInstance[](gameState.playerStates[1].cardsInDeck.length);
-        uint player1cardCount = 0;
-        uint player2cardCount = 0;
+        uint player1CardCount = 0;
+        uint player2CardCount = 0;
 
         for (uint i=0; i < gameState.playerStates.length; i++) {
             for (uint j = 0; j < gameState.playerStates[i].cardsInDeck.length; j++) {
