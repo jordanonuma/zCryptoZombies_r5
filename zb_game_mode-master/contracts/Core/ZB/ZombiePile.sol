d634f247ea95670c2fad8db8a7a747f852c231d0
jordanonuma <jordanonuma@outlook.com>
2020-01-22 06:17:58 -1000

    Set up pseudo-random number generator.

--- zb_game_mode-master/contracts/Core/ZB/ZombiePile.sol
+++ zb_game_mode-master/contracts/Core/ZB/ZombiePile.sol
@@ -47,7 +47,7 @@ contract ZombiePile is ZBGameMode  {
 
         for (uint i=0; i < gameState.playerStates.length; i++) {
             for (uint j = 0; j < gameState.playerStates[i].cardsInDeck.length; j++) {
-                
+                uint rand = uint(keccak256(abi.encodePacked(now, player1CardCount + player2CardCount))) % 2;
             } //end for (uint j)
             changes.changePlayerCardsInDeck(Player(i), newCards, cardCount);
         } //end for (uint i)
