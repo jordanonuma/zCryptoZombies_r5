b3efc77c9d6725c47cb52cbc1bfbed6b21007895
jordanonuma <jordanonuma@outlook.com>
2020-01-23 06:01:21 -1000

    Added check if player1 gets more than half of the total.

--- zb_game_mode-master/contracts/Core/ZB/ZombiePile.sol
+++ zb_game_mode-master/contracts/Core/ZB/ZombiePile.sol
@@ -48,6 +48,9 @@ contract ZombiePile is ZBGameMode  {
         for (uint i=0; i < gameState.playerStates.length; i++) {
             for (uint j = 0; j < gameState.playerStates[i].cardsInDeck.length; j++) {
                 uint rand = uint(keccak256(abi.encodePacked(now, player1CardCount + player2CardCount))) % 2;
+                if (player1CardCount > (gameState.playerStates[0].cardsInDeck.length + gameState.playerStates[1].cardsInDeck.length)/2) {
+                    
+                } //end if()
                 if (rand == 0) {
                     player1Cards[player1CardCount] = gameState.playerStates[i].cardsInDeck[j];
                     player1CardCount++;
