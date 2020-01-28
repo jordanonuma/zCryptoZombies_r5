bb9d690379473e62eb8989aaf33787999d93905f
jordanonuma <jordanonuma@outlook.com>
2020-01-28 05:59:26 -1000

    Annotated for readability.

--- zb_game_mode-master/test/CryptoZombies.js
+++ zb_game_mode-master/test/CryptoZombies.js
@@ -4,5 +4,5 @@ contract("CryptoZombies", (accounts) => {
     let [alice, bob] = accounts;
     it("should be able to create a new zombie", async () => {
         const contractInstance = await CryptoZombies.new();
-    })
+    }) //end it()
 }) //end contract() and end of test
