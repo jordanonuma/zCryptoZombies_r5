e6a0a6f1622091db3741b6ef3707565f9597859c
jordanonuma <jordanonuma@outlook.com>
2020-01-28 05:59:06 -1000

    Revised case for consistency.

--- zb_game_mode-master/test/CryptoZombies.js
+++ zb_game_mode-master/test/CryptoZombies.js
@@ -3,6 +3,6 @@ const CryptoZombies = artifacts.require("CryptoZombies");
 contract("CryptoZombies", (accounts) => {
     let [alice, bob] = accounts;
     it("should be able to create a new zombie", async () => {
-        const contractInstance = await cryptoZombies.new();
+        const contractInstance = await CryptoZombies.new();
     })
 }) //end contract() and end of test
