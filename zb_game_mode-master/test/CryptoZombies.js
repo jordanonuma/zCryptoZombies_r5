a1df796955d491d1506c73439ceec380ac2a4725
jordanonuma <jordanonuma@outlook.com>
2020-01-30 06:18:09 -1000

    Moved 'contractInstance' to beforeEach().

--- zb_game_mode-master/test/CryptoZombies.js
+++ zb_game_mode-master/test/CryptoZombies.js
@@ -4,11 +4,11 @@ contract("CryptoZombies", (accounts) => {
     let [alice, bob] = accounts;
     let contractInstance;
     beforeEach(async () => {
-
+        contractInstance = await CryptoZombies.new();
     }); //end beforeEach()
 
     it("should be able to create a new zombie", async () => {
-        const contractInstance = await CryptoZombies.new();
+        
         const result = await contractInstance.createRandomZombie(zombieNames[0], {from: alice});
         assert.equal(result.receipt.status, true);
         assert.equal(result.logs[0].args.name, zombieNames[0]);
