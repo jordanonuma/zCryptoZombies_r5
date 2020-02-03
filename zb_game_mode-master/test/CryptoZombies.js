a489bb5b2dea28ae2ebae5f29189c66f281dfe82
jordanonuma <jordanonuma@outlook.com>
2020-02-03 05:57:06 -1000

    Added call to transferFrom() made by bob.

--- zb_game_mode-master/test/CryptoZombies.js
+++ zb_game_mode-master/test/CryptoZombies.js
@@ -36,6 +36,7 @@ contract("CryptoZombies", (accounts) => {
             const result = await contractInstance.createRandomZombie(zombieNames[0], {from: alice});
             const zombieId = result.logs[0].args.zombieId.toNumber();
             await contractInstance.approve(bob, zombieId, {from: alice});
+            await contractInstance.transferFrom(alice, bob, zombieId, {from: bob});
         }) //end it()
         it("should approve and then transfer a zombie when the owner calls transferFrom()", async () => {
             
