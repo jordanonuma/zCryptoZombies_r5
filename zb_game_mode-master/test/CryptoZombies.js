af9a79c6041a18bac3d3b571ae82310c7eaeb7b0
jordanonuma <jordanonuma@outlook.com>
2020-02-05 06:27:25 -1000

    Added call to function that will fast forward the test chain's tracking of time by a day.

--- zb_game_mode-master/test/CryptoZombies.js
+++ zb_game_mode-master/test/CryptoZombies.js
@@ -43,6 +43,8 @@ contract("CryptoZombies", (accounts) => {
         it("should approve and then transfer a zombie when the owner calls transferFrom()", async () => {
             const result = await contractInstance.createRandomZombie(zombieNames[0], {from: alice});
             const zombieId = result.logs[0].args.zombieId.toNumber();
+
+            await time.increase(time.duration.days(1));
             await contractInstance.approve(bob, zombieId, {from: alice});
             await contractInstance.transferFrom(alice, bob, zombieId, {from: alice});
             const newOwner = await contractInstance.ownerOf(zombieId);
